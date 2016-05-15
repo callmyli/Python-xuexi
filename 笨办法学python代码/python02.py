90bd0a10ffcbf213cac9a851d0561a79aa7bf8e8
callmyli <bupapanlove01@126.com>
2016-05-15 15:10:40 +0800

    learnpython01
    第一次同步代码

---
+++ 笨办法学python代码/python02.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read program later
+# Angthing after this # is ignored by python
+
+print ("I could have code like this.")
+
+# print ("This won't run")
+
+
+print("This will run.")
